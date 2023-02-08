d210d8185b44a3a50dc78db841d6abcdbcb8dbf8
Idusha Amandi <amandiperera61@gmail.com>
2023-02-08 06:55:17 +0530

    Children Home controllers

---
+++ private/controllers/Childrenhomedetails.php
@@ -0,0 +1,33 @@
+<?php
+
+class Childrenhomedetails extends Controller{
+    function index()
+    {
+        //code..
+        $errors=array();
+        if(count($_POST)>0){
+            $chome=new Childrenhome();
+            if($chome->validate($_POST)){
+                $arr['Name']=$_POST['Name'];
+                $arr['OwnerName']=$_POST['OwnerName'];
+                $arr['regNo']=$_POST['regNo'];
+                $arr['nic']=$_POST['nic'];
+                $arr['contact1']=$_POST['Contact1'];
+                $arr['contact2']=$_POST['Contact2'];
+                $arr['address']=$_POST['address'];
+                $arr['malnutritioned_children']=$_POST['Malnutrition_children'];
+                $arr['healthy_children']=$_POST['Healthy_children'];
+                $arr['areacoordinator_email']=$_POST['areacoordinator_email'];
+                
+                $chome->insert($arr);
+                $this->redirect('home');
+            }else{
+            //error
+            $errors=$chome->errors;
+            }
+        }
+        $this->view('childrenhomedetails',[
+            'errors'=>$errors,
+        ]);
+    }
+}
