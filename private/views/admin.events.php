5717f674be388eb0e1ffcbd099a13e6a9daffc13
Anjuna Serasingha <anjunaserasingha@gmail.com>
2023-02-27 20:50:06 +0530

    event page modified

--- private/views/admin.events.php
+++ private/views/admin.events.php
@@ -16,7 +16,7 @@
         <button type="submit" class="search_logo">
             <i class="fa-solid fa-magnifying-glass "></i>
         </button>
-        
+
     </form>
 
     <div class="blank col-lg-2"></div>
@@ -30,226 +30,55 @@
         <div class="heading-2 p-top-10">Ongoing Events</div>
         <div class="event_scroll p-left-40 p-right-40">
             <div class="grid-9">
-                <!-- EVENT-ONGOING -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
+                <?php if (!empty($ongoing_events)) : ?>
+                    <?php foreach ($ongoing_events as $event) : ?>
+                        <!-- EVENT-ONGOING -->
+                        <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
 
-                    <div class="heading-event txt-al-center m-top-4 m-bottom-4">Event Name</div>
+                            <div class="heading-event txt-al-center m-top-4 m-bottom-4"><?= $event->name ?></div>
 
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
+                            <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
 
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
+                            <div class="event-date txt-al-center txt-08">Event Date - <?= $event->date ?></div>
 
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Donations</div>
+                            <div class="m-top-10">
+                                <div class="row-flex jf-btwn">
+                                    <div class="txt-06 w-semibold txt-purple arrow-icon">Donations</div>
 
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">Rs.25000</div>
+                                    <div class="row-flex">
+                                        <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
+                                        <div class="txt-07 w-semibold txt-purple arrow-icon">Rs.<?= $event->total_amount ?></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-back width-100">
+                                    <div class="progress-fill width-80 height-4px m-top-2"></div>
+                                </div>
+                            </div>
+
+                            <div class="m-top-10">
+                                <div class="row-flex jf-btwn">
+                                    <div class="txt-06 w-semibold txt-purple arrow-icon">Volunteers</div>
+                                    <div class="row-flex">
+                                        <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
+                                        <div class="txt-07 w-semibold txt-purple arrow-icon"><?= $event->no_of_volunteers ?></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-back width-100">
+                                    <div class="progress-fill width-<?= $event->vol_percentage ?> height-4px m-top-2"></div>
+                                </div>
+                            </div>
+
+                            <div class="row-flex jf-center">
+                                <button class="btn btn-green btn-xsm m-top-30">View Details</button>
                             </div>
                         </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Volunteers</div>
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">25</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-ONGOING -->
-
-                <!-- EVENT-ONGOING -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
-
-                    <div class="heading-event txt-al-center m-top-4 m-bottom-4">Event Name</div>
-
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
-
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Donations</div>
-
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">Rs.25000</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Volunteers</div>
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">25</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-ONGOING -->
-
-                <!-- EVENT-ONGOING -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
-
-                    <div class="heading-event txt-al-center m-top-4 m-bottom-4">Event Name</div>
-
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
-
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Donations</div>
-
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">Rs.25000</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Volunteers</div>
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">25</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-ONGOING -->
-
-                <!-- EVENT-ONGOING -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
-
-                    <div class="heading-event txt-al-center m-top-4 m-bottom-4">Event Name</div>
-
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
-
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Donations</div>
-
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">Rs.25000</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Volunteers</div>
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">25</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-ONGOING -->
-
-                <!-- EVENT-ONGOING -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
-
-                    <div class="heading-event txt-al-center m-top-4 m-bottom-4">Event Name</div>
-
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
-
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Donations</div>
-
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">Rs.25000</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="m-top-10">
-                        <div class="row-flex jf-btwn">
-                            <div class="txt-06 w-semibold txt-purple arrow-icon">Volunteers</div>
-                            <div class="row-flex">
-                                <img class="width-25px height-25px" src="<?= ROOT ?>/images/Icons/ArrowLog.png" alt="" srcset="">
-                                <div class="txt-07 w-semibold txt-purple arrow-icon">25</div>
-                            </div>
-                        </div>
-
-                        <div class="progress-back width-100">
-                            <div class="progress-fill width-80 height-4px m-top-2"></div>
-                        </div>
-                    </div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-ONGOING -->
-
+                        <!--end of EVENT-ONGOING -->
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <div class="col-12 w-semibold txt-al-center txt-12 txt-gray">No ongoing events</div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
@@ -268,90 +97,28 @@
         <div class="heading-2 p-top-10">Completed Events</div>
         <div class="event_scroll p-left-40 p-right-40">
             <div class="grid-9">
-                <!-- EVENT-COMPLETED -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
+                <?php if (!empty($ongoing_events)) : ?>
+                    <?php foreach ($ongoing_events as $event) : ?>
+                        <!-- EVENT-COMPLETED -->
+                        <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
 
-                    <div class="heading-event txt-al-center m-top-5 m-bottom-5">Event Name</div>
+                            <div class="heading-event txt-al-center m-top-5 m-bottom-5"><?= $event->name ?></div>
 
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
+                            <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
 
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
+                            <div class="event-date txt-al-center txt-08">Event Date - <?= $event->date ?></div>
 
-                    <div class="event-date txt-al-center txt-12 m-top-30 w-bold">Completed</div>
+                            <div class="event-date txt-al-center txt-12 m-top-30 w-bold">Completed</div>
 
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-block btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-COMPLETED -->
-
-                <!-- EVENT-COMPLETED -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
-
-                    <div class="heading-event txt-al-center m-top-5 m-bottom-5">Event Name</div>
-
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
-
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
-
-                    <div class="event-date txt-al-center txt-12 m-top-30 w-bold">Completed</div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-block btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-COMPLETED -->
-
-                <!-- EVENT-COMPLETED -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
-
-                    <div class="heading-event txt-al-center m-top-5 m-bottom-5">Event Name</div>
-
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
-
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
-
-                    <div class="event-date txt-al-center txt-12 m-top-30 w-bold">Completed</div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-block btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-COMPLETED -->
-
-                <!-- EVENT-COMPLETED -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
-
-                    <div class="heading-event txt-al-center m-top-5 m-bottom-5">Event Name</div>
-
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
-
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
-
-                    <div class="event-date txt-al-center txt-12 m-top-30 w-bold">Completed</div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-block btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-COMPLETED -->
-
-                <!-- EVENT-COMPLETED -->
-                <div class="card-simple event-card-on col-lg-3 col-md-3 p-20 p-top-10">
-
-                    <div class="heading-event txt-al-center m-top-5 m-bottom-5">Event Name</div>
-
-                    <img class="event-image m-bottom-4" src="/img.jpeg" alt="" srcset="">
-
-                    <div class="event-date txt-al-center txt-08">Event Date - 12/11/2022</div>
-
-                    <div class="event-date txt-al-center txt-12 m-top-30 w-bold">Completed</div>
-
-                    <div class="row-flex jf-center">
-                        <button class="btn btn-green btn-block btn-xsm m-top-30">View Details</button>
-                    </div>
-                </div>
-                <!--end of EVENT-COMPLETED -->
+                            <div class="row-flex jf-center">
+                                <button class="btn btn-green btn-block btn-xsm m-top-30">View Details</button>
+                            </div>
+                        </div>
+                        <!--end of EVENT-COMPLETED -->
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <div class="col-12 w-semibold txt-al-center txt-12 txt-gray">No completed events</div>
+                <?php endif; ?>
             </div>
         </div>
 
