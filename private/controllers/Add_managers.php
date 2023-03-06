f53b9c615b7075a196489954b65e7cd65b938ed9
Akila Dharmadasa <akiladharmadasa1.1@gmail.com>
2023-03-06 15:39:11 +0530

    adding images for events,bugs not fixed

--- private/controllers/Add_managers.php
+++ private/controllers/Add_managers.php
@@ -3,7 +3,10 @@ class Add_managers extends Controller
 {
     function index(){
         $manager = new Eventmanager();
-        $data = $manager->where('org_gov_reg_no',$_SESSION['USER']->gov_reg_no);
+        $org = new Organization();
+        $org_data = $org->where('gov_reg_no',$_SESSION['USER']->gov_reg_no);
+        $org_data = $org_data[0];
+        // $data = $manager->where('org_gov_reg_no',$_SESSION['USER']->gov_reg_no);
         // $this->view('shop_org.view', ['allitems' => $data]);
 
         if(count($_POST)>0){  
@@ -15,8 +18,19 @@ class Add_managers extends Controller
             $arr['email'] = $_POST['email'];
             $arr['nic'] = $_POST['nic'];
             // $arr['password'] = password_hash($_POST['pw'],PASSWORD_DEFAULT);
-            $arr['password'] = $_POST['password'];
+            // $arr['password'] = $_POST['password'];
+            $arr['password'] = password_hash($_POST['password'],PASSWORD_DEFAULT);
             $manager->insert($arr);
+
+            $mail = new Mail();
+            $receipient = $arr['email'];
+            $subject = "Account Activation FoodForALL";
+            $message = "Hello ". $arr['full_name'] . "!\r\nWe would like to inform you that you 
+            have been added as an Event Manager for the organization:" . $org_data->name . " at FoodForAll.\r\n
+            Please use your credentials given bellow (with your email) to loging to your account.\r\nPassword:
+            ". $_POST['password'] .  "\r\n\r\n\r\n (Note:It is recomended to change your password after loging in.)";
+
+            $mail->send_mail($receipient, $subject, $message);
 
     }else{
         // echo "hello error";
