6170303fe1f5188b448c77e6a031644a9e50ee3b
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2023-03-06 10:10:52 +0530

    shop

--- private/controllers/Shop.php
+++ private/controllers/Shop.php
@@ -6,6 +6,7 @@ class Shop extends Controller
     {
         $item = new Merchandise_item();
         $org = new Organization();
+
         if (Auth::logged_in()) {
             if (isset($_GET['product_id'])) {
                 $item_id = $_GET['product_id'];
@@ -39,7 +40,7 @@ class Shop extends Controller
                 if (is_array($cart_data)) {
 
                     foreach ($cart_data as $key => $row) {
-                        print_r($_SESSION['CART']);
+                        // print_r($_SESSION['CART']);
                         foreach ($_SESSION['CART'] as $i) {
                             if ($row->item_no == $i['id']) {
                                 $cart_data[$key]->qty = $i['qty'];
@@ -49,7 +50,6 @@ class Shop extends Controller
                     }
                 }
 
-                // print_r($data);
                 $this->view('cart', ['data' => $data[0], 'cart_data' => $cart_data]);
 
             } else if (isset($_GET['id'])) {
@@ -75,7 +75,8 @@ class Shop extends Controller
 
                     //$this->view('shop', ['rows' => $search_data]);
                 }
-
+                // print_r($_SESSION['CART']);
+                // print_r($data);
                 $this->view('shop', ['rows' => $data, 'org_data' => $org_data[0], 'max' => $max, 'min' => $min]);
 
             } else {
@@ -88,121 +89,237 @@ class Shop extends Controller
     public function add_to_cart()
     {
         $item = new Merchandise_item();
-        $cart = new Shopping_cart();
 
         if (isset($_GET['item'])) {
+
+            $timeout = 1 * 600;
+            
+            if (isset($_SESSION['CART']) && isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY']) > $timeout) {
+                foreach ($_SESSION['CART'] as $key => $product) {
+
+                    $items_in_cart = $_SESSION['CART'][$key]['qty'];
+                    // print_r($_SESSION['CART']);
+                    // $current_stock = $_SESSION['CART'][$key]['s'];
+                    $current_stock = "SELECT stock from merchandise_item where item_no= :id";
+                    $arr = ['id' => $product['id']];
+                    $current_stock = $item->query($current_stock, $arr);
+                    $current_stock = $current_stock[0]->stock;
+
+                    $query = 'UPDATE merchandise_item stock set stock= :s where item_no= :id';
+                    $arr = ['s' => $current_stock + $items_in_cart,
+                        'id' => $product['id']];
+                    $updated_stock = $item->query($query, $arr);
+                    // unset($_SESSION['CART'][$key]);
+
+                }
+
+                echo ("<i class='fa-solid fa-circle-exclamation'></i>&nbsp;&nbsp;Shopping cart has been reset due to inactivity:");
+
+                $_SESSION['CART'] = array();
+
+                // $_SESSION['CART'] = array_values($_SESSION['CART']);
+            }
+
+            $_SESSION['LAST_ACTIVITY'] = time();
+
+            // if(date('H:i:s') - $start_time>='10:00')
             $data = $_GET['item'];
             $data = explode(' ', $data);
-            // print_r($data);
-            $item_id = $data[0];
+            $qty=intVal($data[2]);
+            $data = explode('=', $data[0]);
+            $item_id = $data[1];
+           
+            // print_r($qty);
             $item_data = $item->where('item_no', $item_id);
             $item_data = $item_data[0];
+            $update = array();
             if ($item_data) {
                 if (isset($_SESSION['CART'])) {
                     $ids = array_column($_SESSION['CART'], "id");
                     if (in_array($item_data->item_no, $ids)) {
                         $key = array_search($item_data->item_no, $ids);
-                        $_SESSION['CART'][$key]['qty']++;
+                        $query = 'UPDATE merchandise_item stock set stock= :s where item_no= :id';
+                        $arr = ['s' => $item_data->stock - $qty,
+                            'id' => $item_data->item_no];
+                        $updated_stock = $item->query($query, $arr);
+                        $_SESSION['CART'][$key]['qty']+=$qty;
 
                     } else {
                         $arr = array();
                         $arr['id'] = $item_data->item_no;
-                        $arr['qty'] = 1;
+                        $query = 'UPDATE merchandise_item stock set stock= :s where item_no= :id';
+                        $arr = ['s' => $item_data->stock - $qty,
+                            'id' => $item_data->item_no];
+                        $updated_stock = $item->query($query, $arr);
+                        $arr['qty'] = $qty;
                         $_SESSION['CART'][] = $arr;
+
                     }
                 } else {
+
                     $arr = array();
                     $arr['id'] = $item_data->item_no;
-                    $arr['qty'] = 1;
+                    $query = 'UPDATE merchandise_item stock set stock= :s where item_no= :id';
+                    $arr = ['s' => $item_data->stock - $qty,
+                        'id' => $item_data->item_no];
+                    $updated_stock = $item->query($query, $arr);
+                    $arr['qty'] = $qty;
                     $_SESSION['CART'][] = $arr;
+
                 }
             }
-            print_r($item_data);
-            print_r($_SESSION['CART']);
+            // print_r($item_data);
+            // print_r($_SESSION['CART']);
 
-            $this->redirect('shop?id=' . $data[1]);
+            // $this->redirect('shop?id=' . $data[1]);
         }
         echo ('<i class="fa-solid fa-circle-check"></i>&nbsp;&nbsp;item added to cart sucessfully!');
     }
 
     public function add_qty()
     {
-        $params = $_GET['cartid'];
+        $product = new Merchandise_item();
+        if (isset($_GET['cart'])) {
+            $params = $_GET['cart'];
 
-        $params = explode(' ', $params);
-        $id = $params[1];
-        if (isset($_SESSION['CART'])) {
-            foreach ($_SESSION['CART'] as $key => $item) {
+            $params = explode(' ', $params);
+            $params = explode('=', $params[1]);
 
-                if ($item['id'] == $id) {
-                    $_SESSION['CART'][$key]['qty'] += 1;
-                    break;
+            // print_r($params);
+            $id = $params[0];
+            $item_data = $product->where('item_no', $id);
+            $item_data = $item_data[0];
+            if (isset($_SESSION['CART'])) {
+                foreach ($_SESSION['CART'] as $key => $item) {
+
+                    if ($item['id'] == $id) {
+                        $query = 'UPDATE merchandise_item stock set stock= :s where item_no= :id';
+                        $arr = ['s' => $item_data->stock - 1,
+                            'id' => $item_data->item_no];
+                        $updated_stock = $product->query($query, $arr);
+                        $_SESSION['CART'][$key]['qty'] += 1;
+                        echo ($_SESSION['CART'][$key]['qty']);
+                        break;
+                    }
+
                 }
-
             }
         }
-        $this->redirect('shop?cartid=' . $params[0]);
+
+        // $this->redirect('shop?cartid=' . $params[0]);
     }
 
     public function sub_qty()
     {
-        $params = $_GET['cartid'];
 
-        $params = explode(' ', $params);
-        $id = $params[1];
-        if (isset($_SESSION['CART'])) {
-            foreach ($_SESSION['CART'] as $key => $item) {
+        if (isset($_GET['cart'])) {
+            $product = new Merchandise_item();
+            $params = $_GET['cart'];
 
-                if ($item['id'] == $id) {
-                    if ($_SESSION['CART'][$key]['qty'] == 0) {
+            $params = explode(' ', $params);
+            $params = explode('=', $params[1]);
+            // print_r($params);
+            $id = $params[0];
+            $item_data = $product->where('item_no', $id);
+            $item_data = $item_data[0];
+            if (isset($_SESSION['CART'])) {
+                foreach ($_SESSION['CART'] as $key => $item) {
+                    if ($item['id'] == $id) {
+                        if ($_SESSION['CART'][$key]['qty'] == 0) {
+                            echo (0);
+                            unset($_SESSION['CART'][$key]);
+                            $_SESSION['CART'] = array_values($_SESSION['CART']);
+                            break;
+                        } else {
+                            $query = 'UPDATE merchandise_item stock set stock= :s where item_no= :id';
+                            $arr = ['s' => $item_data->stock + 1,
+                                'id' => $item_data->item_no];
+                            $updated_stock = $product->query($query, $arr);
+                            $_SESSION['CART'][$key]['qty'] -= 1;
+                            if ($_SESSION['CART'][$key]['qty'] == 0) {
+                                echo (0);
+                                unset($_SESSION['CART'][$key]);
+                                $_SESSION['CART'] = array_values($_SESSION['CART']);
+                                break;
+                            }
+                            echo ($_SESSION['CART'][$key]['qty']);
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+        // $this->redirect('shop?cartid=' . $params[0]);
+    }
+    public function delete_qty()
+    {
+
+        if (isset($_GET['cart'])) {
+            // $items_in_cart = $_POST['items_in_cart'];
+            $product = new Merchandise_item();
+            $params = $_GET['cart'];
+            $params = explode(' ', $params);
+            $params = explode('=', $params[1]);
+            // print_r($params);
+            $id = $params[0];
+            $item_data = $product->where('item_no', $id);
+            $item_data = $item_data[0];
+            if (isset($_SESSION['CART'])) {
+                foreach ($_SESSION['CART'] as $key => $item) {
+                    if ($item['id'] == $id) {
+                        $items_in_cart = $_SESSION['CART'][$key]['qty'];
+                        $query = 'UPDATE merchandise_item stock set stock= :s where item_no= :id';
+                        $arr = ['s' => $item_data->stock + $items_in_cart,
+                            'id' => $item_data->item_no];
+                        $updated_stock = $product->query($query, $arr);
                         unset($_SESSION['CART'][$key]);
                         $_SESSION['CART'] = array_values($_SESSION['CART']);
                         break;
                     }
-                    $_SESSION['CART'][$key]['qty'] -= 1;
-                    break;
                 }
             }
         }
-        $this->redirect('shop?cartid=' . $params[0]);
-    }
-    public function delete_qty()
-    {
-        $params = $_GET['cartid'];
-
-        $params = explode(' ', $params);
-        $id = $params[1];
-        if (isset($_SESSION['CART'])) {
-            foreach ($_SESSION['CART'] as $key => $item) {
-
-                if ($item['id'] == $id) {
-                    unset($_SESSION['CART'][$key]);
-                    $_SESSION['CART'] = array_values($_SESSION['CART']);
-                    break;
-
-                }
-            }
-        }
-        $this->redirect('shop?cartid=' . $params[0]);
+        // $this->redirect('shop?cartid=' . $params[0]);
     }
     public function update_qty()
     {
-        
-        if ($json = file_get_contents('php://input')) {
-            $json = file_get_contents('php://input');
-            $data = json_decode($json);
-            $quantity = intval($data->quantity);
-            $index = $data->index;
-            
-            //  $_SESSION['CART'][$index]->qty=$quantity;
-            // $_SESSION['CART'][$index]->qty=$quantity;
-            $_SESSION['CART'][$index]['qty']=$quantity;
-            print_r( $_SESSION['CART']);
-           
+        $item = new Merchandise_item();
+        //get number added to the input
+        $data = json_decode(file_get_contents("php://input"), true);
+        $quantity = $data["quantity"];
+        $index = $data["index"];
 
-            // Do something with $myVariable
-        
+        //if stock's aren't available
+        if ($quantity > $_SESSION['CART'][$index]['s']) {
+            $available = $_SESSION['CART'][$index]['s'] + 1;
+            echo ("<i class='fa-solid fa-circle-exclamation'></i>&nbsp;&nbsp; Available " . $available . " only");
+            exit();
         }
+        // print_r($_SESSION['CART']);
+        //getting cart quantity and inserted quantity
+        $id = $_SESSION['CART'][$index]['id'];
+        $current_qty = $_SESSION['CART'][$index]['qty'];
+        $new_qty = $quantity - $current_qty;
 
+        //get remaining stock
+        $stock_left = "SELECT stock from merchandise_item where item_no= :id";
+        $arr = ['id' => $id];
+        $stock_left = $item->query($stock_left, $arr);
+        $stock_left = $stock_left[0]->stock;
+
+        $query = "update merchandise_item set stock= :s where item_no=:id";
+        // echo($new_qty);
+        if ($new_qty >= 0) {
+            $arr = ['id' => $id,
+                's' => $stock_left - $new_qty];
+            $item->query($query, $arr);
+        } else if ($new_qty < 0) {
+            $arr = ['id' => $id,
+                's' => $stock_left + (-1 * $new_qty)];
+            $item->query($query, $arr);
+        }
+        $_SESSION['CART'][$index]['qty'] = $quantity;
+        // print_r($quantity.$index.$current_qty);
     }
 }
--- private/views/cart.php
+++ private/views/cart.php
@@ -4,7 +4,8 @@
 <link rel="stylesheet" href="<?=ROOT?>/assets/cart.css">
 
 <h2><?=$data->name?> Merch Store</h2>
-<div class="corner"><i class="fa-solid fa-cart-shopping fa-xl"></i><a href="<?=ROOT?>/shop?id=<?=$data->gov_reg_no?>"><button>Continue Shopping</button></a></div>
+<div class="corner"><a href=""><div class="clear"><i class="fa-solid fa-trash-can fa-xl"></i></div></a><div><i class="fa-solid fa-cart-shopping fa-xl"></i><a href="<?=ROOT?>/shop?id=<?=$data->gov_reg_no?>"><button>Continue Shopping</button></a></div></div>
+<div class="alert" id="alert"><i class="fa-solid fa-circle-exclamation">&nbsp;&nbsp;</i>Available 13 only</div>
 <div class="cart_table">
     <table>
         <tr>
@@ -13,22 +14,27 @@
             <th>Price</th>
             <th>Total</th>
         </tr>
-        <?php if($cart_data):?>
-        <?php $i=0?>
-        <?php foreach($cart_data as $rows):?>
-        <tr>
+        <?php if ($cart_data): ?>
+        <?php $i = 0?>
+        <?php $total = 0?>
+        <?php foreach ($cart_data as $rows): ?>
+        <tr class="table_row">
             <td><?=$cart_data[$i]->name?><br><img src="images/merch_items/<?=$cart_data[$i]->image?>"></td>
-            <td><a href="<?=ROOT?>/shop/add_qty?cartid=<?=$data->gov_reg_no?>+<?=$cart_data[$i]->item_no?>" class="cart_quantity_up">+</a><input type="text" value="<?=$cart_data[$i]->qty?> " class="qty"><a class="cart_quantity_down" href="<?=ROOT?>/shop/sub_qty?cartid=<?=$data->gov_reg_no?>+<?=$cart_data[$i]->item_no?>">-</a></td>
-            <td><?=$cart_data[$i]->price?></td>
-            <td><?=$cart_data[$i]->price*$cart_data[$i]->qty?><a href="<?=ROOT?>/shop/delete_qty?cartid=<?=$data->gov_reg_no?>+<?=$cart_data[$i]->item_no?>"><button>Remove</button></a></td>
+            <td><a href="<?=ROOT?>/shop/add_qty?cart=<?=$data->gov_reg_no?>+<?=$cart_data[$i]->item_no?>" class="cart_quantity_up" >+</a><input type="text" value="<?=$cart_data[$i]->qty?> " class="qty"><a class="cart_quantity_down" href="<?=ROOT?>/shop/sub_qty?cart=<?=$data->gov_reg_no?>+<?=$cart_data[$i]->item_no?>" >-</a></td>
+            <td><span class="price"><?=$cart_data[$i]->price?></span></td>
+            <td><span class="total"><?=$cart_data[$i]->price * $cart_data[$i]->qty?></span><a href="<?=ROOT?>/shop/delete_qty?cart=<?=$data->gov_reg_no?>+<?=$cart_data[$i]->item_no?>" class="remove"><button >Remove</button></a></td>
         </tr>
+        <?php $total += $cart_data[$i]->price * $cart_data[$i]->qty?>
         <?php $i++?>
-        <?php endforeach ?>
+        <?php endforeach?>
+        <?php endif?>
+        <?php if (!$cart_data): ?>
+            <td class="empty"><i class="fa-solid fa-bag-shopping"></i>Your cart looks empty!</td>
         <?php endif?>
     </table>
 </div>
 <div class="down">
-    <div class="row">Bill Total <span>Rs.<span></div>
+    <div class="row" >Bill Total <span id="bill_total">Rs. <?php if (isset($total)) {echo ($total);} else {echo "0";}?>.00<span></div>
     <div class="row">Delivery Charges<span>Rs.<span></div>
     <div class="row">Total Amount<span>Rs.<span></div>
 </div>
