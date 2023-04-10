411ad383b182c902990d512303304fa441170665
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2023-04-10 18:25:27 +0530

    signup select

---
+++ private/controllers/Select.php
@@ -0,0 +1,10 @@
+<?php
+class Select extends Controller
+{
+    public function index()
+    {
+        $this->view('select');
+
+    }
+   
+}
--- private/core/functions.php
+++ private/core/functions.php
@@ -20,7 +20,7 @@ function esc($var)
 
 function stripe_checkout($data)
 {
-
+ 
     require '../private/models/stripe-php-master/init.php';
 
     $stripe = new \Stripe\StripeClient('sk_test_51Mkpm2KZpyK8KvAZPZUbc2YT3M7NSnfuXhjrZI5QpKCMeHDwY7p97hGQN4EItsLfdNS5JB4v4xHuskzONTsRv4cf004Mpw8QQV');
@@ -44,7 +44,40 @@ function stripe_checkout($data)
         ],
 
         'success_url' => 'http://localhost/food_for_all/public/thanks',
-        'cancel_url' => 'http://localhost:4242/cancel',
+        'cancel_url' => 'http://localhost/food_for_all/public/cancel',
+    ]);
+
+    header("HTTP/1.1 303 See Other");
+    header("Location: " . $checkout_session->url);
+
+}
+function donate_checkout($data)
+{
+ 
+    require '../private/models/stripe-php-master/init.php';
+
+    $stripe = new \Stripe\StripeClient('sk_test_51Mkpm2KZpyK8KvAZPZUbc2YT3M7NSnfuXhjrZI5QpKCMeHDwY7p97hGQN4EItsLfdNS5JB4v4xHuskzONTsRv4cf004Mpw8QQV');
+
+    $checkout_session = $stripe->checkout->sessions->create([
+        'line_items' => [[
+            'price_data' => [
+                'currency' => 'lkr',
+                'product_data' => [
+                    'name' => 'Donation#'.$data['_id'],
+                    'description'=>'Delivered to: '.$data['name'].','.$data['address']
+                ],
+                'unit_amount' => $data['subtotal']*100,
+            ],
+            'quantity' => 1,
+        ]],
+        'mode' => 'payment',
+        'metadata'=>[
+            'user_id'=>$data['user_id'],
+            'date'=>$data['date']
+        ],
+
+        'success_url' => 'http://localhost/food_for_all/public/thanks',
+        'cancel_url' => 'http://localhost/food_for_all/public/cancel',
     ]);
 
     header("HTTP/1.1 303 See Other");
