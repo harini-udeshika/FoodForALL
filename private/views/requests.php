712300c422fd48620d016a6dedc60c0438cb6ebc
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2023-05-01 14:42:31 +0530

    small fixes

--- private/views/requests.php
+++ private/views/requests.php
@@ -4,11 +4,22 @@
 <?php $this->view('includes/submenu')?>
 <div class="container">
     <h3>Volunteer Requests</h3>
-    <?php $i=0?>
-    <?php foreach($data as $d):?>
-   <div class="row"><p><?=$data[$i]->name?></p><p><?=$data[$i]->volunteer_type?></p><p><?=$data[$i]->date_time?></p><p><i class="fa-solid fa-hourglass-start"></i>&nbsp;&nbsp;<?=$data[$i]->message?></p></div>
-   <?php $i++?>
-   <?php endforeach?>
+    <?php $i = 0?>
+<?php foreach ($data as $d): ?>
+<div class="row"><p><?=$data[$i]->name?></p><p><?=$data[$i]->volunteer_type?></p>
+    <p><?=$data[$i]->date_time?></p>
+    <?php if ($data[$i]->message=="pending"):?>
+    <p><i class="fa-solid fa-hourglass-start" style="color: #8c8fd9;"></i>&nbsp;&nbsp;<?=$data[$i]->message?></p>
+    <?php endif;?>
+    <?php if ($data[$i]->message=="accepted"):?>
+    <p><i class="fa-solid fa-circle-check" style="color: #7fc25b;"></i>&nbsp;&nbsp;<?=$data[$i]->message?></p>
+    <?php endif;?>
+    <?php if ($data[$i]->message=="rejected"):?>
+    <p><i class="fa-solid fa-circle-xmark" style="color: #e72354;"></i>&nbsp;&nbsp;<?=$data[$i]->message?></p>
+    <?php endif;?>
+</div>
+<?php $i++?>
+<?php endforeach?>
 </div>
 <script src=" navbar.js"></script>
-<?php $this->view('includes/footer')?> 
+<?php $this->view('includes/footer')?>
