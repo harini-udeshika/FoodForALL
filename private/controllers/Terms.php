8cf322cf18415bba457df7e1b082c1fb28d21078
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2023-05-02 23:59:12 +0530

    small changes

---
+++ private/controllers/Terms.php
@@ -0,0 +1,11 @@
+<?php
+class Terms extends Controller
+{
+
+    public function index()
+    {
+       $this->view('terms');
+
+    }
+
+}
---
+++ private/views/terms.php
@@ -0,0 +1,24 @@
+<?php $this->view('includes/header')?>
+<link rel="stylesheet" href="<?=ROOT?>/assets/terms.css">
+<h1>Terms and Conditions</h1>
+<div class="container">
+<h3>Introduction</h3>
+<p>These terms and conditions ("Terms") govern your use of the FoodForALL
+    website ("Website") and the services provided by the charity organizations through the Website.
+    By registering with the FoodForALL platform, you agree to be bound by these Terms.</p>
+    <h3>Registration</h3>
+    <p>To register with FoodForALL, you must provide accurate and complete information about yourself. You must also ensure that your 
+        login details, including your username and password, are kept confidential and secure.</p>
+    <h3>Donations</h3>
+    <p>By donating to the charity organizations, you confirm that you are the rightful owner of the funds and that the donation is made voluntarily and without coercion. The charity organization will use the donation for the purposes specified on the Website, 
+        and you agree that the charity organization has the right to allocate the funds at its discretion.</p>
+    <h3>Volunteering</h3>
+    <p>By volunteering with the charity organizations, you confirm that you are doing so voluntarily and without coercion. You acknowledge that the activities to volunteer will be provided by the organizations, and you should only request to volunteer if you are comfortable with the tasks. The charity organizations does not guarantee that you will be accepted as a volunteer, and it is up to the event 
+        managers assigned to each event by the organizations to accept or reject your volunteering requests.</p>
+    <h3>User Conduct</h3>
+    <p>You agree to use the Website and the charity organization's services in compliance with applicable laws and regulations. You also agree to behave in a manner that is respectful and courteous to other users of the Website and the charity organization's services. You agree not to engage in any activity that could harm the charity organization or its reputation, 
+        including but not limited to, hacking, data breaches, or spamming.</p>
+    <h3>Intellectual Property</h3>
+    <p>All intellectual property rights in the Website and the charity organization's services, including but not limited to, trademarks, logos, copyrights, and trade secrets, are owned by the charity organization or its licensors. You may not use, reproduce, 
+        or distribute any of these materials without the prior written consent of the charity organization.</p>
+</div>
