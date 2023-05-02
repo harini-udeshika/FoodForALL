c80826dd666654dac9b2f3c4ee4cd49c442375f4
Amandi130 <93074050+Amandi130@users.noreply.github.com>
2023-05-02 17:13:46 +0530

    modified modles

--- private/models/Elderhome.php
+++ private/models/Elderhome.php
@@ -14,7 +14,7 @@ class Elderhome extends Model{
         if (empty($DATA["OwnerName"])) {
             $this->errors['OwnerName'] = "Owner name with initials required";
         }
-        if (empty($DATA['regNo'])) {
+        if (empty($DATA['regNo']) && empty($DATA['elderid'])) {
             $this->errors['regNo'] = "Registration number is required";
         }
         if (empty($DATA["nic"])) {
@@ -26,10 +26,20 @@ class Elderhome extends Model{
         if (empty($DATA["address"])) {
             $this->errors['address'] = "Address is required";
         }
-        
-        if($this->where('regNo',$DATA['regNo'])){
-            $this->errors['regNo']='This Elder house already exit';
+
+        if (empty($DATA["Members"])) {
+            $this->errors['Members'] = "Number of elders are required";
         }
+
+        if (isset($DATA['regNo']) && $this->where('regNo', $DATA['regNo'])) {
+            $this->errors['regNo'] = 'This elders Home already exists';
+        } 
+        
+        $total_members = array($DATA['Cholesterol_patients'], $DATA['Healthy_adults'], $DATA['Diabetes_patients'],$DATA['Both_patients']);
+        if (array_sum($total_members) != $DATA['Members']) {
+            $this->errors['Members'] = 'This family numbers do not match';
+        }
+        
 
         if (count($this->errors) == 0) {
             return true;
