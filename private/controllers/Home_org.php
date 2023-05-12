8af744f4e50726b6a40ba3a4f56a44427142583e
Akila Dharmadasa <akiladharmadasa1.1@gmail.com>
2023-05-12 20:59:17 +0530

    changes to uis

--- private/controllers/Home_org.php
+++ private/controllers/Home_org.php
@@ -6,8 +6,6 @@ class Home_org extends Controller
         $data = $user->where('gov_reg_no',$_SESSION['USER']->gov_reg_no);
         $data = $data[0];
 
-
-        
         $event = new Event();
         
         $org_reg = $_SESSION['USER']->gov_reg_no;
@@ -27,15 +25,38 @@ class Home_org extends Controller
         $query = "SELECT * FROM event WHERE org_gov_reg_no= :id && approved!=0 && date<CURRENT_DATE";
         $arr = ['id'=>$org_reg];
         $past = $event->query($query,$arr);
-        // echo "<br>past";
-        // print_r($past);
-        // die;
-
-        // $query = "SELECT * FROM event WHERE org_gov_reg_no= :id && approved!=0";
-        // $arr = ['id'=>$org_reg];
         $allevents = $user->selectAll($org_reg);
 
-        $this->view('home_org.view',['pending' => $pending, 'ongoin' => $ongoing, 'past' => $past , 'rows' => $data, 'allevents' => $allevents]);
+        $sub_data = $this->isSubscribed();
+
+        $this->view('home_org.view',['pending' => $pending, 'ongoin' => $ongoing, 'past' => $past , 'rows' => $data, 'allevents' => $allevents,
+                                    'sub_data'=>$sub_data]);
+    }
+
+    public function isSubscribed()
+    {
+        $org = new Subscribe();
+        $query = "SELECT * FROM subscription Where org_gov_reg_no= :id 
+        && status = 1 ORDER BY date DESC";
+
+        $arr_2 = ['id' => Auth::gov_reg_no()];
+        $data = $org->query($query,$arr_2);
+
+        if ($data) {
+            $paid_date = new DateTime($data[0]->date);
+            $today = new DateTime(); 
+            $interval = $paid_date->diff($today);
+            $days = $interval->days;
+
+            if ($days > 30) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+        // die;
     }
    
 }
